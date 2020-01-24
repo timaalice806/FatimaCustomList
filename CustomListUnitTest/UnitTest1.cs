3fed007a49cfff66834dba335afdb3d7ec16085a
fatima <tima.alice806@gmail.com>
2020-01-24 08:09:22 -0600

    began unit tests for forthcoming custom built add method

--- CustomListUnitTest/UnitTest1.cs
+++ CustomListUnitTest/UnitTest1.cs
@@ -8,8 +8,34 @@ namespace CustomListUnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckValueIndexZero()
         {
+            //ARRANGE
+            TimaList<int> myList = new TimaList<int>();
+            int value = 6;
+            int expected = 6;
+            int actual;
+
+            //ACT
+            myList.Add(value);
+            actual = myList[0];
+
+            //ASSERT
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckCountAfterAdd()
+        {
+            //ARRANGE
+            TimaList<int> myList = new TimaList<int>();
+
+            //ACT
+            int value = 6;
+            int expected = 1;
+            int actual;
+
+            //ASSERT
+            Assert.AreEqual(expected, actual);
         }
     }
 }

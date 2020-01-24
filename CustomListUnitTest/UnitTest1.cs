c7acd29b76714c81bf2187c778a859565855d55a
fatima <tima.alice806@gmail.com>
2020-01-24 15:13:09 -0600

    added two more unit tests for the add method and cleaned up previous test

--- CustomListUnitTest/UnitTest1.cs
+++ CustomListUnitTest/UnitTest1.cs
@@ -28,14 +28,43 @@ namespace CustomListUnitTest
         {
             //ARRANGE
             TimaList<int> myList = new TimaList<int>();
-
-            //ACT
             int value = 6;
             int expected = 1;
-            int actual;
+            //ACT
+            myList.Add(value);
 
             //ASSERT
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, myList.Count);
+        }
+        [TestMethod]
+        public void CheckOrderOfIndicesAfterAdd()
+        {
+            //ARRANGE
+            TimaList<int> myList = new TimaList<int>();
+            int value = 1;
+            int secondValue = 2;
+            int thirdValue = 3;
+
+            //ACT
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList[0] = value;
+            myList[1] = secondValue;
+            myList[2] = thirdValue;
+
+            //ASSERT
+            Assert.AreEqual(thirdValue, myList[2]);
+        }
+        public void CheckArrayCapacity()
+        {
+            //ARRANGE
+            TimaList<int> myList = new TimaList<int>();
+
+            //ACT
+
+            //ASSERT
+
         }
     }
 }

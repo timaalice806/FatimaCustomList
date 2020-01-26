cbfada9013a95c657eff581f138882b071ed5449
fatima <tima.alice806@gmail.com>
2020-01-26 11:26:33 -0600

    began add method logic and testing my unit tests

--- TimaList.cs
+++ TimaList.cs
@@ -8,6 +8,40 @@ namespace FatimasCustomList
 {
     public class TimaList<T>
     {
+        //MEMBER VARIABLES (MY LIST HAS A..)
 
+        //CREATE EMPTY GENERIC ARRAY
+        T[] items = new T[Capacity]
+
+        int count = 0;
+        int capacity = 4;
+
+        public TimaList()
+        {
+            items = new T[Capacity];
+
+            {
+                get { return items[i]; }
+                set { items[i] = value; }
+            }
+        }
+        //MEMBER METHOD (MY LIST CAN DO..)
+        public void Add(T items)
+        {
+            //ADD ITEMS TO ARRAY
+            items[Count] = item;
+
+            //INCREMENT THE COUNT BY 1
+            Count++
+
+            //LOOP THROUGH ARRAY
+            for (int i = 0; i < Count; i++);
+
+            if(count == capacity)
+            {
+                capacity*= 2
+            }
+   
+        }
     }
 }

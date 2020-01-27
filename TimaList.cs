ece3dc9baaedf3b4cc1fd6494a0afc30b2db6d87
fatima <tima.alice806@gmail.com>
2020-01-27 12:01:01 -0600

    added capacity and count properties using get & set, also duplicated capacity and set original items array to new duplicated array

--- TimaList.cs
+++ TimaList.cs
@@ -10,38 +10,79 @@ namespace FatimasCustomList
     {
         //MEMBER VARIABLES (MY LIST HAS A..)
 
-        //CREATE EMPTY GENERIC ARRAY
-        T[] items = new T[Capacity]
+        T[] items = new T[4];
+        T[] tempItems;
 
-        int count = 0;
-        int capacity = 4;
-
-        public TimaList()
+        int count;
+        public int Count
         {
-            items = new T[Capacity];
-
+            get
             {
-                get { return items[i]; }
-                set { items[i] = value; }
+                return count;
+            }
+            set
+            {
+                count = value;
             }
         }
+        int capacity;
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+        //INDEXER
+        public T this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
+
+        //CONSTRUCTOR (ALL OF MY LISTS WILL HAVE..)
+        public TimaList()
+        {
+            count = 0;
+            capacity = 4;
+            items = new T[Capacity];
+
+        }
+
         //MEMBER METHOD (MY LIST CAN DO..)
-        public void Add(T items)
+        public void Add(T item)
         {
             //ADD ITEMS TO ARRAY
             items[Count] = item;
 
             //INCREMENT THE COUNT BY 1
-            Count++
+            Count++;
 
             //LOOP THROUGH ARRAY
             for (int i = 0; i < Count; i++);
 
+            //ONCE COUNT = CAPACITY, DUPLICATE 
             if(count == capacity)
             {
-                capacity*= 2
+                capacity *= 2;
+                tempItems = items;
+                items = new T[capacity];
+
+                for (int i = 0; i < tempItems.Length; i++)
+                {
+                    items[i] = tempItems[i];
+                }
             }
-   
         }
     }
 }
